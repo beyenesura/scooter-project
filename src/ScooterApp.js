520336cd4d038a283cf1e4cb50cb2d1fbab3059c
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-02-21 14:55:56 -0500

    Update ScooterApp.js

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -2,7 +2,31 @@ const User = require('./User')
 const Scooter = require('./Scooter')
 
 class ScooterApp {
-  // ScooterApp code here
+  constructor() {
+    this.stations = []; 
+    this.registeredUsers = {}; 
+    this.currentUser = null; 
+  }
+
+  registerUser(user) {
+    if (user.username in this.registeredUsers) {
+      throw new Error('Username already taken');
+    }
+    this.registeredUsers[user.username] = user;
+  }
+
+  loginUser(username, password) {
+    const user = this.registeredUsers[username];
+    if (!user || user.password !== password) {
+      throw new Error('Invalid username or password');
+    }
+    this.currentUser = user;
+  }
+
+  logoutUser() {
+    this.currentUser = null;
+  }
 }
+
 
 module.exports = ScooterApp
